9d18d0a573cb3136efabd87f2dd72b32678ac7db
Rabiu Kabir <rabiu.kabir@aun.edu.ng>
2023-01-17 09:24:15 +0100

    update

--- index.js
+++ index.js
@@ -77,5 +77,5 @@ return Joi.validate(book, schema);
 }
  
 //PORT ENVIRONMENT VARIABLE
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
